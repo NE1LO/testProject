c61b8e9826fb427032125a4346ca0a2b0b21d7b7
Anton Smeiko <asmeiko13@gmail.com>
2024-06-02 23:18:53 +0200

    validate form

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-Install dependencies
+## Install dependencies
+
 Run the following command to install all project dependencies: npm install
 
-Start the project
+## Start the project
+
 After successfully installing dependencies, start the project with: npm run dev
--- src/Page/Setting.vue
+++ src/Page/Setting.vue
@@ -1,25 +1,179 @@
 <template>
   <div class="w-full bg-white px-[24px] py-[24px] rounded-[12px]">
     <h2 class="text-[20px] font-[600] pb-[32px]">Kontaktní údaje</h2>
-    <FormContacts />
-    <h2 class="text-[20px] font-[600] pb-[44px] pt-[32px]">Fakturační údaje</h2>
-    <FormInvoiceData />
-    <button
-      class="setting-btn mt-[32px] px-[24px] py-[12px] text-[#ffff] rounded-[8px] transition-colors duration-300"
-      type="submit"
-    >
-      Uložit změny
-    </button>
+    <form @submit.prevent="validateForm">
+      <div class="flex gap-[12px]">
+        <div class="flex flex-col gap-[12px] w-full">
+          <InputLabel
+            inputId="userName"
+            labelText="Jméno"
+            :inputType="'name'"
+            v-model="formData.userName"
+          />
+          <InputLabel
+            inputId="userEmail"
+            labelText="Email"
+            :inputType="'email'"
+            v-model="formData.userEmail"
+          />
+        </div>
+        <div class="flex flex-col gap-[12px] w-full">
+          <InputLabel
+            inputId="userLastName"
+            labelText="Příjmení"
+            :inputType="'text'"
+            v-model="formData.userLastName"
+          />
+          <InputLabel
+            inputId="userPhoneNumber"
+            labelText="Telefon"
+            :inputType="'tel'"
+            v-model="formData.userPhoneNumber"
+          />
+        </div>
+      </div>
+      <h2 class="text-[20px] font-[600] pb-[44px] pt-[32px]">
+        Fakturační údaje
+      </h2>
+      <div class="flex gap-[12px]">
+        <div class="flex flex-col gap-[12px] w-full">
+          <InputLabel
+            inputId="companyName"
+            labelText="Název firmy"
+            inputType="text"
+            v-model="formData.companyName"
+          />
+          <InputLabel
+            inputId="city"
+            labelText="Město"
+            inputType="text"
+            v-model="formData.city"
+          />
+          <InputLabel
+            inputId="ic"
+            labelText="IČ"
+            inputType="text"
+            v-model="formData.ic"
+          />
+          <InputLabel
+            inputId="firstName"
+            labelText="Jméno"
+            inputType="name"
+            v-model="formData.firstName"
+          />
+          <InputLabel
+            inputId="phone"
+            labelText="Telefon"
+            inputType="tel"
+            v-model="formData.phone"
+          />
+        </div>
+        <div class="flex flex-col gap-[12px] w-full">
+          <InputLabel
+            inputId="street"
+            labelText="Ulice"
+            inputType="text"
+            v-model="formData.street"
+          />
+          <InputLabel
+            inputId="zipcode"
+            labelText="PSČ"
+            inputType="text"
+            v-model="formData.zipcode"
+          />
+          <InputLabel
+            inputId="vat"
+            labelText="DIČ"
+            inputType="text"
+            v-model="formData.vat"
+          />
+          <InputLabel
+            inputId="lastName"
+            labelText="Příjmení"
+            inputType="text"
+            v-model="formData.lastName"
+          />
+          <InputLabel
+            inputId="email"
+            labelText="Email"
+            inputType="email"
+            v-model="formData.email"
+          />
+        </div>
+      </div>
+      <button
+        class="setting-btn mt-[32px] px-[24px] py-[12px] text-[#ffff] rounded-[8px] transition-colors duration-300"
+        type="submit"
+      >
+        Uložit změny
+      </button>
+      <div v-if="errorMessage" class="text-red-500 mt-[16px]">
+        {{ errorMessage }}
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import FormContacts from "../components/FormContacts.vue";
-import FormInvoiceData from "../components/FormInvoiceData.vue";
+import InputLabel from "../components/InputLabel.vue";
+
 export default {
   components: {
-    FormContacts,
-    FormInvoiceData,
+    InputLabel,
+  },
+  data() {
+    return {
+      formData: {
+        userName: "",
+        userEmail: "",
+        userLastName: "",
+        userPhoneNumber: "",
+        companyName: "",
+        city: "",
+        ic: "",
+        firstName: "",
+        phone: "",
+        street: "",
+        zipcode: "",
+        vat: "",
+        lastName: "",
+        email: "",
+      },
+      errorMessage: "",
+    };
+  },
+  methods: {
+    validateForm() {
+      this.errorMessage = "";
+      const requiredFields = [
+        "userName",
+        "userEmail",
+        "userLastName",
+        "userPhoneNumber",
+        "companyName",
+        "city",
+        "ic",
+        "firstName",
+        "phone",
+        "street",
+        "zipcode",
+        "vat",
+        "lastName",
+        "email",
+      ];
+
+      for (let field of requiredFields) {
+        if (!this.formData[field]) {
+          this.errorMessage = "Please fill out all required fields.";
+          return;
+        }
+      }
+
+      this.submitForm();
+    },
+    submitForm() {
+      console.log("Form submitted:", this.formData);
+    },
   },
 };
 </script>
