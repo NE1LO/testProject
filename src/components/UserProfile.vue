c2645d8a094b0d46a92c3ba2c099ed2204d8469c
Anton Smeiko <asmeiko13@gmail.com>
2025-04-15 12:09:02 +0200

    Update UserProfile.vue

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="flex flex-col justify-center items-center pt-[32px] pb-[24px]">
-    <h3 class="text-[22px] font-[600] leading-[28px]">Danil Filatov</h3>
-    <p class="text font-[400] leading-[22px] text-[14px]">
-      danil.filatov@newlogic.cz
-    </p>
+    <h3 class="text-[22px] font-[600] leading-[28px]">Your User</h3>
+    <p class="text font-[400] leading-[22px] text-[14px]">example</p>
   </div>
 </template>
 
